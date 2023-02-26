3cf76c1a5f4f0930655377e13ac5c1bb7852e298
Ruan Tiengo <ruantiengo@gmail.com>
2023-02-26 19:58:23 -0300

    fix: add process.env.port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app)
   const swagger = new SwaggerDocumentation(app)
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
